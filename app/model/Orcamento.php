95a05e8211a4006501e49faae070554b48c3f2d7
Weydans C. Barros <weydans@hotmail.com>
2019-04-18 08:29:58 -0300

    Correção de links nas views, criação do banco de dados e criação da classe de modelo Orcamento.php

---
+++ app/model/Orcamento.php
@@ -0,0 +1,45 @@
+<?php
+
+class Orcamento extends Model
+{
+	private static $table = 'orcamento';
+	private static $columns = [
+		'orcamento_id',
+		'orcamento_cliente',
+		'orcamento_data_hora',
+		'orcamento_vendedor',
+		'orcamento_descricao',
+		'orcamento_valor'
+	];
+
+	public function save(array $data)
+	{
+		return $this->saveModel(self::$table, self::$columns, $data);
+	}
+
+	public function update(array $data)
+	{
+		return $this->updateModel(self::$table, self::$columns, $data);
+	}
+
+	public function find(int $id)
+	{
+		return $this->findModel(self::$table, self::$columns, $id);
+	}
+
+	public function listAll()
+	{
+		return $this->listAllModel(self::$table);
+	}
+
+	public function delete(int $id)
+	{
+		return $this->deleteModel(self::$table, self::$columns, $id);
+	}
+
+	public function nextId()
+	{
+		return $this->nextIdModel(self::$table, self::$columns);
+	}
+
+}
