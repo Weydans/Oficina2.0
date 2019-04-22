908381b22c83e5444b4971f94c14366aad582a09
Weydans C. Barros <weydans@hotmail.com>
2019-04-22 16:52:02 -0300

    Especialização da classe Paginator para poder executar buscas personalizadas e buscas de todos os resultados de uma tabela

--- core/class/Paginator.php
+++ core/class/Paginator.php
@@ -13,6 +13,7 @@ class Paginator
 	private $resPerPage;
 	private $numPages;
 	private $totalResults;
+	private $paramBusca;
 
 	private $obj;
 	private $tabela;
@@ -21,7 +22,9 @@ class Paginator
 	private $values;
 	private $limit;
 	private $offset;
+	private $ordenacao;
 
+	private $allResults;
 	private $result;
 
 	private $view;
@@ -34,17 +37,16 @@ class Paginator
 	 * @param int $page Recebe o valor da variavel querystring que recebe a pagina atual.
 	 * @param object $obj Recebe um objeto que está sendo manipulado para paginação.
 	 * @param int $page Recebe o número de resultados que serão exibidos por pagina.
+	 * @param string $paramBosca Querystring contendo os valores da pesquisa.
 	 */
-	public function __construct(string $url, int $page, object $obj, int $resPerPage)
+	public function __construct(string $url, $page, object $obj, int $resPerPage, string $paramBusca = null)
 	{ 
 		$this->url = $url;
 		$this->obj = $obj;
 		$this->tabela = $obj->getTable();
 		$this->resPerPage = $resPerPage;
-		$this->totalResults = count($obj->listAll());
-		$this->numPages = ceil($this->totalResults / $resPerPage);
-
-		$this->setPage($page);		
+		$this->page = $page;
+		$this->paramBusca = $paramBusca;
 	}
 
 	/**
@@ -58,13 +60,52 @@ class Paginator
 	 * aos links da query, porem sem serem precedidos de ':' (dois pontos).
 	 * @return array Retorna uma matriz contendo um array associativo com os resultados;
 	 */
-	public function exePaginator(array $campos = array(), string $condicao = null, array $values = array()) : array
+	public function exePaginator(array $campos = array(), string $condicao = null, array $values = array(), string $ordenacao = null) : array
 	{
-		$this->pager();
+		$this->ordenacao = $ordenacao;
+
+		$this->setParamBusca();
 		$this->setCampos($campos);
-		$this->setCondicao($condicao);
 		$this->setValues($values);
-		$this->read();
+		$this->setCondicao($condicao);
+		$this->setTotalResults();
+		$this->setNumPages();
+		$this->setPage($this->page);	
+
+		$this->pager();
+		//$this->read();
+		$this->setResultBusca();
+
+		return $this->result;
+	}
+
+	/**
+	 * <b>exePaginator</b>:
+	 * Ordena e centraliza a execução dos métodos responsáveis pela paginação.
+	 * Obs: os links das condições devem ser compátiveis com as chaves do array $values, 
+	 * porem, com os valores precedidos de ':' (dois pontos);
+	 * @param array $campos Recebe os campos informados pelo programador.
+	 * @param string $condicao Recebe a condição de leitura no banco.
+	 * @param array $values Recebe um array associativo noqual as chaves devem ser identicas
+	 * aos links da query, porem sem serem precedidos de ':' (dois pontos).
+	 * @return array Retorna uma matriz contendo um array associativo com os resultados;
+	 */
+	public function exeBusca(array $campos = array(), string $condicao = null, array $values = array(), string $ordenacao = null) : array
+	{
+		$this->ordenacao = $ordenacao;
+
+		$this->setParamBusca();
+		$this->setCampos($campos);
+		$this->setValues($values);
+		$this->setCondicao($condicao);
+		$this->setTotalResults();
+		$this->setNumPages();
+		$this->setPage();	
+
+		$this->pager();
+		//$this->read();
+
+		$this->setResultBusca();
 
 		return $this->result;
 	}
@@ -84,24 +125,54 @@ class Paginator
 		$numLinks = (!empty($numLinks)) ? $numLinks : $numLinks = 5;
 
 		$this->view = "<ul class='paginator {$this->styleClass}'>";
-		$this->view .= "<a href='{$this->url}1'><li><span>{$first}<span></li></a>";
+		$this->view .= "<a href='{$this->url}1{$this->paramBusca}'><li><span>{$first}<span></li></a>";
 
 		for ($i = $this->page - $numLinks + 1; $i < $this->page  ; $i++) { 
 			if ($i > 0)
-				$this->view .= "<a href='{$this->url}{$i}'><li>{$i}</li></a>";			
+				$this->view .= "<a href='{$this->url}{$i}{$this->paramBusca}'><li>{$i}</li></a>";			
 		}
 
 		$this->view .= "<li class='active'>{$i}</li>";
 
 		for ($i = $this->page; $i < $this->page + $numLinks ; $i++) { 
 			if ($i > $this->page && $i <= $this->numPages)
-				$this->view .= "<a href='{$this->url}{$i}'><li>{$i}</li></a>";
+				$this->view .= "<a href='{$this->url}{$i}{$this->paramBusca}'><li>{$i}</li></a>";
 		}
 
-		$this->view .= "<a href='{$this->url}{$this->numPages}'><li><span>{$last}<span></li></a>";
+		$this->view .= "<a href='{$this->url}{$this->numPages}{$this->paramBusca}'><li><span>{$last}<span></li></a>";
 		$this->view .= "</ul>";
 
 		return $this->view;
+	}
+
+	/**
+	 * <b>getAllResults</b>
+	 * @return array Retorna uma matraiz de arrays associativos com todos os resultados. 
+	 */
+	public function getAllResults()
+	{
+		return $this->allResults;
+	}
+
+	/**
+	 * <b>getTotalResults</b> 
+	 * @return int Retorna o total de registros encontrados.
+	 */
+	public function getTotalResults()
+	{
+		return $this->totalResults;
+	}
+
+	/**
+	 * <b>setParamBusca</b>
+	 * Configura o atributo $this->paramBusca 
+	 * como uma string vazia caso nao seja informado nenhum valor.
+	 */
+	private function setParamBusca()
+	{
+		if (empty($this->paramBusca)){
+			$this->paramBusca = '';
+		}
 	}
 
 	/**
@@ -119,15 +190,19 @@ class Paginator
 	 * Cnfigura o atributo $this->page.
 	 * @param int $page Valor a ser validado para atributo $this->page.
 	 */
-	private function setPage(int $page)
+	private function setPage()
 	{
-		if ($page > $this->numPages){ 
+		if (empty($this->page) || !is_numeric($this->page)){
+			$url = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+			$this->page = 1;
+
+		} elseif ($this->page > $this->numPages){ 
 			$this->page = $this->numPages;
 			header("Location: {$this->url}{$this->numPages}");
 
 		} else {
-			if ($page > 0){
-				$this->page = $page;
+			if ($this->page > 0){
+				$this->page = $this->page;
 			} else {
 				$this->page = 1;
 			}
@@ -165,6 +240,31 @@ class Paginator
 	}
 
 	/**
+	 * <b>setTotalResults</b>:
+	 * Configura o total de resultados encontrados na paginação. 
+	 */
+	private function setTotalResults()
+	{
+		try {			
+			$sql = new sql;
+			$this->allResults = $sql->select("SELECT {$this->campos} FROM {$this->tabela} {$this->condicao} {$this->ordenacao}", $this->values);
+			$this->totalResults = count($this->allResults);
+
+		} catch (Exception $e) {
+			echo Msg::setMsg( ($e->getMessage() . ' :: ' . $e->getFile() . ' :: ' . $e->getLine()) , ERROR);
+		}
+	}
+
+	/**
+	 * <b>setNumPages</b>:
+	 * Configura o total de paginas da paginação. 
+	 */
+	private function setNumPages()
+	{
+		$this->numPages = ceil($this->totalResults / $this->resPerPage);
+	}
+
+	/**
 	 * <b>setValues</b>
 	 * Prepara os valores que vão substituir os links da query do prepared statement,
 	 * acrecenta ':'(dois pontos) as chaves do array para substituição por links.
@@ -176,11 +276,8 @@ class Paginator
 		if (count($values) < 1){
 			$this->values = array();
 		} else {
-			$this->values = prepareData($values);
+			$this->values = $this->prepareData($values);
 		}
-
-		$this->values[':limit'] = (string) $this->limit; 
-		$this->values[':offset'] = (string) $this->offset;
 	}
 
 	/**
@@ -200,22 +297,25 @@ class Paginator
 		}
 
 	}
+	
+ 	/**
+ 	 * <b>setResultBusca</b>:
+ 	 * Configura o resultado da busca de acordo com o resultado da paginação. 
+ 	 */
+ 	private function setResultBusca()
+ 	{		
+ 		$i = $this->offset;
+ 		$this->result = array();
 
-	/**
-	 * <b>read</b>:
-	 * Realiza a leitura das colunas selecionadas dinamicamente em uma determinada tabela
-	 * Respeitando os valores de limit e ofset.
-	 */
-	private function read()
-	{
-		try {
-			$sql = new Sql;
-			$this->result = $sql->select("SELECT {$this->campos} FROM {$this->tabela} {$this->condicao} LIMIT :limit OFFSET :offset", $this->values);
-
-		} catch (Exception $e) {
-			echo Msg::setMsg($e->getMessage(), ERROR);
-		}
-	}
+ 		if (is_array($this->allResults)){
+ 			foreach ($this->allResults as $key => $value) {
+ 				if ($key >= $this->offset && $key < $this->offset + $this->limit){
+ 					$this->result[] = $this->allResults[$i];
+ 					$i++;		
+ 				}
+ 			}
+ 		}
+ 	}
 
 	/**
      * <b>prepareData</b>:
